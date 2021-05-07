cf9280320a0916b31299b7b7f482b406dfeaf377
paraglande03 <paraglande03@gmail.com>
2021-05-07 19:49:43 +0530

    Added : Parking car function to owner controller

--- src/main/java/com/parking/ParkingService/controller/OwnerController.java
+++ src/main/java/com/parking/ParkingService/controller/OwnerController.java
@@ -2,15 +2,14 @@ package com.parking.ParkingService.controller;
 
 
 import com.parking.ParkingService.dto.ResponseDto;
+import com.parking.ParkingService.dto.VehicleDTO;
 import com.parking.ParkingService.model.Vehicle;
 import com.parking.ParkingService.service.OwnerService;
+import com.parking.ParkingService.service.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,13 +20,22 @@ public class OwnerController {
     @Autowired
     private OwnerService ownerService;
 
+    @Autowired
+    private VehicleService vehicleService;
+
     @GetMapping("/vehicle/all")
     public ResponseEntity<ResponseDto> allVehicles(){
         ResponseDto responseDto=new ResponseDto("All vehicles:", ownerService.getAllVehicles());
         return new ResponseEntity<ResponseDto>(responseDto,HttpStatus.OK);
     }
 
-   
+    @PostMapping("/park")
+    public ResponseEntity<ResponseDto> parkVehicle(@RequestBody VehicleDTO vehicleDTO){
+
+        ResponseDto responseDto=new ResponseDto("Vehicle Parked in Lot "+vehicleDTO.getLotId()+" slot "+ vehicleDTO.getSlotId() ,vehicleService.addVehicle(vehicleDTO));
+        return  new   ResponseEntity<ResponseDto>(responseDto, HttpStatus.OK);
+    }
+
 
 
 }
